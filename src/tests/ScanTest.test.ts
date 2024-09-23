053a50f93593b9f1748908ecc1042b70f48b952f
miryamfoiferCX <miryam.foifer@checkmarx.com>
2024-09-23 14:58:54 +0300

    Update Vorpal Name to ASCA(AST-65647) (#759)
    * Update Vorpal Name to ASCA
    
    * update ast-cli to pre-release version
    
    * update ast-cli to pre-release version - 2
    
    * Track Checkmarx CLI binaries with Git LFS
    
    * Update checkmarx-ast-cli to 2.2.6
    
    ---------
    
    Co-authored-by: github-actions <github-actions@github.com>
    Co-authored-by: AlvoBen <144705560+AlvoBen@users.noreply.github.com>
    Co-authored-by: AlvoBen <alvo@post.bgu.ac.il>

--- src/tests/ScanTest.test.ts
+++ src/tests/ScanTest.test.ts
@@ -1,14 +1,12 @@
+import { CxWrapper } from '../main/wrapper/CxWrapper';
 import { CxCommandOutput } from "../main/wrapper/CxCommandOutput";
 import { CxParamType } from "../main/wrapper/CxParamType";
 import { BaseTest } from "./BaseTest";
-import CxWrapperFactory from "../main/wrapper/CxWrapperFactory";
-
-const cxWrapperFactory = new CxWrapperFactory();
 
 describe("ScanCreate cases", () => {
     const cxScanConfig = new BaseTest();
     it('ScanList Successful case', async () => {
-        const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
+        const auth = new CxWrapper(cxScanConfig);
         const cxCommandOutput: CxCommandOutput = await auth.scanList("");
         console.log(" Json object from scanList successful case: " + JSON.stringify(cxCommandOutput));
         expect(cxCommandOutput.payload.length).toBeGreaterThan(1);
@@ -22,7 +20,7 @@ describe("ScanCreate cases", () => {
         params.set(CxParamType.FILTER, "*.ts,!**/node_modules/**/*");
         params.set(CxParamType.BRANCH, "master");
         params.set(CxParamType.SCAN_TYPES,"kics");
-        const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
+        const auth = new CxWrapper(cxScanConfig);
         const cxCommandOutput: CxCommandOutput = await auth.scanCreate(params);
         const scanObject = cxCommandOutput.payload.pop();
         const scanShowObject = await auth.scanShow(scanObject.id);
@@ -37,7 +35,7 @@ describe("ScanCreate cases", () => {
         params.set(CxParamType.SAST_PRESET_NAME, "Checkmarx Default Fake");
         params.set(CxParamType.BRANCH, "master");
         params.set(CxParamType.SCAN_TYPES, "sast");
-        const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
+        const auth = new CxWrapper(cxScanConfig);
         const cxCommandOutput: CxCommandOutput = await auth.scanCreate(params);
         const scanObject = cxCommandOutput.payload.pop();
         const scanShowObject = await auth.scanShow(scanObject.id);
@@ -52,7 +50,7 @@ describe("ScanCreate cases", () => {
         params.set(CxParamType.FILTER, "*.ts,!**/node_modules/**/*");
         params.set(CxParamType.BRANCH, "master");
         params.set(CxParamType.ADDITIONAL_PARAMETERS, "--scan-types sast");
-        const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
+        const auth = new CxWrapper(cxScanConfig);
         const cxCommandOutput: CxCommandOutput = await auth.scanCreate(params);
         const scanObject = cxCommandOutput.payload.pop();
         const scanShowObject = await auth.scanShow(scanObject.id);
@@ -68,7 +66,7 @@ describe("ScanCreate cases", () => {
         params.set(CxParamType.SAST_PRESET_NAME, "Checkmarx Default Fake");
         params.set(CxParamType.ADDITIONAL_PARAMETERS, "--async");
         params.set(CxParamType.BRANCH, "master");
-        const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
+        const auth = new CxWrapper(cxScanConfig);
         const cxCommandOutput: CxCommandOutput = await auth.scanCreate(params);
         const scanObject = cxCommandOutput.payload.pop();
         const scanShowObject = await auth.scanShow(scanObject.id);
@@ -83,7 +81,7 @@ describe("ScanCreate cases", () => {
         params.set(CxParamType.BRANCH, "master");
         params.set(CxParamType.FILTER, "*.ts,!**/node_modules/**/*");
         params.set(CxParamType.ADDITIONAL_PARAMETERS, "--async");
-        const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
+        const auth = new CxWrapper(cxScanConfig);
         const cxCommandOutput: CxCommandOutput = await auth.scanCreate(params);
         const scanObject = cxCommandOutput.payload.pop();
         await auth.scanCancel(scanObject.id)
@@ -92,7 +90,7 @@ describe("ScanCreate cases", () => {
     })
 
     it('KicsRealtime Successful case ', async () => {
-        const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
+        const auth = new CxWrapper(cxScanConfig);
         const [outputProcess,pid] = await auth.kicsRealtimeScan("dist/tests/data/Dockerfile","docker","-v");
         const cxCommandOutput: CxCommandOutput = await outputProcess;
         console.log(" Json object from successful no wait mode case: " + JSON.stringify( cxCommandOutput.payload));
@@ -103,7 +101,7 @@ describe("ScanCreate cases", () => {
     })
 
     it('ScaRealtime Successful case', async () => {
-        const wrapper = await cxWrapperFactory.createWrapper(cxScanConfig);
+        const wrapper = new CxWrapper(cxScanConfig);
         const cxCommandOutput: CxCommandOutput = await wrapper.runScaRealtimeScan(process.cwd());
         if(cxCommandOutput.exitCode == 1) {
             expect(cxCommandOutput.payload).toBeUndefined();
@@ -115,58 +113,58 @@ describe("ScanCreate cases", () => {
 
     it("Should check if scan create is possible", async() => {
         const cxScanConfig = new BaseTest();
-        const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
+        const auth = new CxWrapper(cxScanConfig);
         const tenantSettings: boolean = await auth.ideScansEnabled();
         expect(tenantSettings).toBeDefined();
     })
 
     it("Should check if AI guided remediation is active", async() => {
         const cxScanConfig = new BaseTest();
-        const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
+        const auth = new CxWrapper(cxScanConfig);
         const aiEnabled: boolean = await auth.guidedRemediationEnabled();
         expect(aiEnabled).toBeDefined();
     })
 
-    // it('ScanVorpal fail case Without extensions', async () => {
-    //     const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
-    //     const cxCommandOutput: CxCommandOutput = await auth.scanVorpal("tsc/tests/data/python-file");
-    //     console.log(" Json object from failure case: " + JSON.stringify(cxCommandOutput));
-    //
-    //     expect(cxCommandOutput.payload[0].error.description).toEqual("The file name must have an extension.");
-    //     expect(cxCommandOutput.exitCode).toBe(0);
-    //     expect(cxCommandOutput.payload[0].status).toBeUndefined();
-    // });
+    it('ScanAsca fail case Without extensions', async () => {
+        const auth = new CxWrapper(cxScanConfig);
+        const cxCommandOutput: CxCommandOutput = await auth.scanAsca("tsc/tests/data/python-file");
+        console.log(" Json object from failure case: " + JSON.stringify(cxCommandOutput));
 
-    // it('ScanVorpal Successful case', async () => {
-    //     const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
-    //     const cxCommandOutput: CxCommandOutput = await auth.scanVorpal("tsc/tests/data/python-vul-file.py");
-    //     console.log("Json object from scanVorpal successful case: " + JSON.stringify(cxCommandOutput));
-    //     const scanObject = cxCommandOutput.payload.pop();
-    //     expect(cxCommandOutput.payload).toBeDefined();
-    //     expect(cxCommandOutput.exitCode).toBe(0);
-    //     expect(scanObject.status).toEqual(true);
-    // });
-    //
-    // it('ScanVorpal with complex name Successful case', async () => {
-    //     const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
-    //     const cxCommandOutput: CxCommandOutput = await auth.scanVorpal("tsc/tests/data/var express = require('express';.js");
-    //     console.log("Json object from scanVorpal successful case: " + JSON.stringify(cxCommandOutput));
-    //     const scanObject = cxCommandOutput.payload.pop();
-    //     expect(cxCommandOutput.payload).toBeDefined();
-    //     expect(cxCommandOutput.exitCode).toBe(0);
-    //     expect(scanObject.status).toEqual(true);
-    // });
-    //
-    // it('ScanVorpal Successful case with update version', async () => {
-    //     const auth = await cxWrapperFactory.createWrapper(cxScanConfig);
-    //     const cxCommandOutput: CxCommandOutput = await auth.scanVorpal("tsc/tests/data/python-vul-file.py", true);
-    //     console.log("Json object from scanVorpal successful case with update version: " + JSON.stringify(cxCommandOutput));
-    //     const scanObject = cxCommandOutput.payload.pop();
-    //     expect(cxCommandOutput.payload).toBeDefined();
-    //     expect(cxCommandOutput.exitCode).toBe(0);
-    //     expect(scanObject.status).toEqual(true);
-    //     expect(Number.isInteger(scanObject.scanDetails[0].line)).toBe(true);
-    //     expect(typeof scanObject.scanDetails[0].description).toBe('string');
-    // });
+        expect(cxCommandOutput.payload[0].error.description).toEqual("The file name must have an extension.");
+        expect(cxCommandOutput.exitCode).toBe(0);
+        expect(cxCommandOutput.payload[0].status).toBeUndefined();
+    });
+
+    it('ScanAsca Successful case', async () => {
+        const auth = new CxWrapper(cxScanConfig);
+        const cxCommandOutput: CxCommandOutput = await auth.scanAsca("tsc/tests/data/python-vul-file.py");
+        console.log("Json object from scanAsca successful case: " + JSON.stringify(cxCommandOutput));
+        const scanObject = cxCommandOutput.payload.pop();
+        expect(cxCommandOutput.payload).toBeDefined();
+        expect(cxCommandOutput.exitCode).toBe(0);
+        expect(scanObject.status).toEqual(true);
+    });
+
+    it('ScanAsca with complex name Successful case', async () => {
+        const auth = new CxWrapper(cxScanConfig);
+        const cxCommandOutput: CxCommandOutput = await auth.scanAsca("tsc/tests/data/var express = require('express';.js");
+        console.log("Json object from scanAsca successful case: " + JSON.stringify(cxCommandOutput));
+        const scanObject = cxCommandOutput.payload.pop();
+        expect(cxCommandOutput.payload).toBeDefined();
+        expect(cxCommandOutput.exitCode).toBe(0);
+        expect(scanObject.status).toEqual(true);
+    });
+
+    it('ScanAsca Successful case with update version', async () => {
+        const auth = new CxWrapper(cxScanConfig);
+        const cxCommandOutput: CxCommandOutput = await auth.scanAsca("tsc/tests/data/python-vul-file.py", true);
+        console.log("Json object from scanAsca successful case with update version: " + JSON.stringify(cxCommandOutput));
+        const scanObject = cxCommandOutput.payload.pop();
+        expect(cxCommandOutput.payload).toBeDefined();
+        expect(cxCommandOutput.exitCode).toBe(0);
+        expect(scanObject.status).toEqual(true);
+        expect(Number.isInteger(scanObject.scanDetails[0].line)).toBe(true);
+        expect(typeof scanObject.scanDetails[0].description).toBe('string');
+    });
 
 });
